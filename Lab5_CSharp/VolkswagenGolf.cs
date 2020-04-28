e12ed4fae3da289a11c7d269e032f4af59f3943f
Ivan Ilyushchenka <52858229+IvanSir@users.noreply.github.com>
2020-04-28 20:31:48 +0300

    NEW

--- Lab5_CSharp/VolkswagenGolf.cs
+++ Lab5_CSharp/VolkswagenGolf.cs
@@ -1,14 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System;
 
 namespace _3cSharp
 {
     class VolkswagenGolf : Car
     {
-        public VolkswagenGolf(int fuel,bool isBroken, string color, int distance) : base(fuel, isBroken: isBroken, color: color, length: 2, height: 2, weight: 100, distance: distance)
+        public VolkswagenGolf(int fuel, bool isBroken, string color, int distance) : base(fuel, isBroken: isBroken, color: color, length: 2, height: 2, weight: 100, distance: distance)
         {
             Speed = 50;
             MaxFuel = 120;
@@ -21,20 +17,9 @@ namespace _3cSharp
         public override void PrintInfo()
         {
             Console.WriteLine("Car : Volkswagen Golf");
-            Console.WriteLine("Type : "+TypeOfCar);
+            Console.WriteLine("Type : " + TypeOfCar);
             base.PrintInfo();
         }
 
-        public override Vehicle AddNewVehicle()
-        {
-            Console.WriteLine("Color : ");
-            string color = Console.ReadLine();
-            Console.WriteLine("Distance : ");
-            NumberCheck(Console.ReadLine(), out int distance);
-            Console.WriteLine("Fuel(in L) : ");
-            NumberCheck(Console.ReadLine(), out int fuel);
-            bool izBroken = DefineBool(Console.ReadLine());
-            return new VolkswagenGolf(fuel,izBroken,color,distance);
-        }
     }
 }
