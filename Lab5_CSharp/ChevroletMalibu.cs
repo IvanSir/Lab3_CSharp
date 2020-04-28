67889c1e2eaec6cb5144edf9c85e78f36569c127
Ivan Ilyushchenka <52858229+IvanSir@users.noreply.github.com>
2020-04-28 20:31:16 +0300

    NEW

--- Lab5_CSharp/ChevroletMalibu.cs
+++ Lab5_CSharp/ChevroletMalibu.cs
@@ -1,14 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System;
 
 namespace _3cSharp
 {
     class ChevroletMalibu : Car
     {
-        public ChevroletMalibu(int fuel, bool isBroken, string color, int distance) : base(fuel,  isBroken: isBroken, color: color, length: 2, height: 2, weight: 100, distance: distance)
+        public ChevroletMalibu(int fuel, bool isBroken, string color, int distance) : base(fuel, isBroken: isBroken, color: color, length: 2, height: 2, weight: 100, distance: distance)
         {
             Speed = 90;
             MaxFuel = 90;
@@ -23,16 +19,6 @@ namespace _3cSharp
             Console.WriteLine("Type : " + TypeOfCar);
             base.PrintInfo();
         }
-        public override Vehicle AddNewVehicle()
-        {
-            Console.WriteLine("Color : ");
-            string color = Console.ReadLine();
-            Console.WriteLine("Distance : ");
-            NumberCheck(Console.ReadLine(), out int distance);
-            Console.WriteLine("Fuel(in L) : ");
-            NumberCheck(Console.ReadLine(), out int fuel);
-            bool izBroken = DefineBool(Console.ReadLine());
-            return new ChevroletMalibu(fuel, izBroken, color, distance);
-        }
+
     }
 }
