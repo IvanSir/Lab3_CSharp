5e732d164bcd3174ef094e4a01000a74850be525
Ivan Ilyushchenka <ginvaelwf@mail.ru>
2020-05-01 21:56:21 +0300

    NEW

--- Lab6_CSharp/VolvoXC70.cs
+++ Lab6_CSharp/VolvoXC70.cs
@@ -1,15 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System;
+
 
 namespace _3cSharp
 {
     class VolvoXC70 : Car
     {
-        public VolvoXC70(int fuel, bool isBroken, string color, int distance) : 
-            base(fuel, isBroken: isBroken, color:color,length: 2,height:2,weight: 100,distance: distance)
+        public VolvoXC70(int fuel, bool isBroken, string color, int distance) : base(fuel, isBroken: isBroken, color: color, length: 2, height: 2, weight: 100, distance: distance)
         {
             Speed = 60;
             MaxFuel = 100;
@@ -24,19 +20,7 @@ namespace _3cSharp
             Console.WriteLine("Type : " + TypeOfCar);
             base.PrintInfo();
         }
-        public override Car AddNewVehicle()
-        {
-            Console.WriteLine("Color : ");
-            string color = Console.ReadLine();
-            Console.WriteLine("Distance : ");
-            NumberCheck(Console.ReadLine(), out int distance);
-            Console.WriteLine("Fuel(in L) : ");
-            NumberCheck(Console.ReadLine(), out int fuel);
-            bool izBroken = DefineBool(Console.ReadLine());
-            return new VolvoXC70(fuel, izBroken, color, distance);
-        }
+
 
     }
 }
-    
-
